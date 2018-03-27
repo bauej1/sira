465a0d65c7ee94176f4ce8e47c9ea92de5ad32d3
leameierx <meier.lea@icloud.com>
2018-03-27 14:37:51 +0200

    added bodypart_detail layout

--- app/src/main/java/com/sira/sira/MainActivity.java
+++ app/src/main/java/com/sira/sira/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.language);
+        setContentView(R.layout.bodypart_detail);
         Log.d("","");
     }
 }
