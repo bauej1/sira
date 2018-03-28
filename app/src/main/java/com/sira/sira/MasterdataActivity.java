466c10364b6fa4b7110583366dee654931f002cf
leameierx <meier.lea@icloud.com>
2018-03-28 14:30:07 +0200

    designed masterdata-view

---
+++ app/src/main/java/com/sira/sira/MasterdataActivity.java
@@ -0,0 +1,16 @@
+package com.sira.sira;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lea on 28.03.18.
+ */
+
+public class MasterdataActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.masterdata);
+    }
+}
