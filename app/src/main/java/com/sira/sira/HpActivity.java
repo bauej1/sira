ca1676d2e4ee47987dbd7e45783b317a121335d2
bauej1 <jan.bauer@shinternet.ch>
2018-04-03 15:14:03 +0200

    HpActivity

---
+++ app/src/main/java/com/sira/sira/HpActivity.java
@@ -0,0 +1,18 @@
+package com.sira.sira;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jan on 03/04/2018.
+ */
+
+public class HpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hp_aufnahme1);
+
+    }
+}
